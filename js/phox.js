440e422fb71c985545d1ea3e330425163500bd26
david <dblanchard1@bbox.fr>
2016-04-21 01:48:42 +0200

    update registry
    update registry

---
+++ js/phox.js
@@ -0,0 +1,60 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+    
+var cssFiles = [
+    "css/bootstrap.css"
+    , "css/jquery-ui.css"
+    , "css/jquery-ui.structure.css"
+    , "css/jquery-ui.theme.css"
+//    , "css/jquerysctipttop.css"
+//    , "css/multiaccordion.jquery.css"
+    , "css/docs.css"
+//    , "css/prettify.css"
+//    , "css/jumbotron.css"
+    , "css/full-slider.css"    
+];
+
+var jsFiles = [
+      "js/widgets.js"
+    , "js/jquery.js"
+    , "js/jquery-ui.js"
+//    , "js/multiaccordion.jquery.js"
+    , "js/bootstrap.js"
+    , "js/holder.js"
+//    , "js/prettify.js"
+    , "js/application.js"
+//    , "js/spin.min.js"
+    , "js/php.default.min.js"
+//    , "js/jphoenix.js"
+    , "js/code_phoenix.js"
+    , "js/configuration.js"
+//    , "js/drag-and-drop.js"
+    , "app/controllers/main/main.js"
+];
+
+for(var key in cssFiles) {
+    attributes = {
+          href: href
+        , rel: "stylesheet"
+    };
+    
+    var styleSheet = document.createElement("link");
+    for(var key in attributes) {
+        styleSheet.setAttribute(key, attributes[key]);
+    }
+    var head = document.getElementsByTagName("head")[0];
+    head.appendChild(styleSheet);    
+}
+
+for(var key in jsFiles) {
+    var myInclude =  document.createElement("script");
+    myInclude.src = jsFiles[key];
+    myInclude.type = "text/javascript";
+    document.body.appendChild(myInclude);
+}
+
--- tmp/app_controllers_main_main.class.php
+++ tmp/app_controllers_main_main.class.php
@@ -1,4 +1,32 @@
-<!DOCTYPE html>
+<?php
+namespace Phox\Controllers;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of logme
+ *
+ * @author david
+ */
+class Main extends \Phoenix\Web\UI\TControl {
+    //put your code here
+    protected $hostname = SERVER_ROOT;
+
+    public function load() {
+        if(HTTP_HOST == 'localhost') {
+            $this->hostname = 'http://localhost:8001';
+        } else {
+            $this->hostname = 'http://www.ladmin.loc';
+        }
+//        $token = $this->request->getToken();
+//        $result = TAuthentication::getPermissionByToken($token);
+//        if(!$result) {
+//            $this->response->redirect(SERVER_ROOT);
+//        }
+    }
+       	public function createObjects() {	}	public function declareObjects() {	}	public function afterBindingObjects() {	}	public function displayHtml() {?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
@@ -33,10 +61,11 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script type="text/javascript" src="app/phox.js"></script>
-    <script type="text/javascript" src='<phx:echo var="hostname" />/js/ladmin.js'></script>
+    <script type="text/javascript" src='<?php echo $this->hostname; ?>/js/ladmin.js'></script>
 
 </head>
 
 <body data-twttr-rendered="true" data-spy="scroll" >
 </body>
 </html>
+<?php	}}
