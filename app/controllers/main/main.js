8b02a6036d42d75f65a9ad25822fb2318100b07b
dpjb71 <dpjb78@gmail.com>
2017-01-07 17:01:25 +0100

    Little UI changes and JS a fixes

--- app/controllers/main/main.js
+++ app/controllers/main/main.js
@@ -1,32 +1,25 @@
-//sol = TWebApplication.create('www.sol.loc');
-//sol.main = sol.createView('main');
-var solMain = sol.createController(sol.main, 'sol.main')
-.actions({
-    goHome : function () {
-        solMain.getSimpleView('home.html', function(data) {
-            $(document.body).html(data.view);
-        })         
-    }
-    , goHome2 : function () {
-        solMain.getSimpleView('master.html', function(data) {
-            $(document.body).html(data.view);
-            solMain.getSimpleView('home.html', function(data) {
-                $('#homeContent').html(data.view);
-            })
-        });        
-    }
-})
-.onload(function () {
-        //this.origin = TRegistry.item('main').origin + '/';
-//        var the = this;
-//        the.getSimpleView('master.html', function(data) {
-//            $(document.body).html(data.view);
-//            the.getSimpleView('home.html', function(data) {
-//                $('#homeContent').html(data.view);
-//            })
-//        });
-    solMain = this;
-    this.goHome2();
-    ladminIndex.bindEvents();
-});
+var APP_NAME = "sol";
+var sol = null;
+var solMain = null;
+var solHost = (window.location.href.indexOf('localhost') > -1) ? 'localhost:8000' : 'sol.loc';
+Phink.DOM.ready(function () {
 
+    sol = TWebApplication.create(solHost);
+    sol.main = sol.createView('main');
+    solMain = sol.createController(sol.main, 'sol.main')
+    .actions({
+        goHome: function () {
+            solMain.getSimpleView('master.html', function (data) {
+                $(document.body).html(data.view);
+                solMain.getSimpleView('home.html', function (data) {
+                    $('#homeContent').html(data.view);
+                });
+            });
+        }
+    })
+    .onload(function () {
+        solMain = this;
+        this.goHome();
+        //ladminIndex.bindEvents();
+    });
+});
