4e34deb0b79a7585bd318d90c7fa424eb615b857
david <dblanchard1@bbox.fr>
2017-04-02 18:48:16 +0200

    Add docker integration

---
+++ runtime/app_controllers_1_1.class.php
@@ -0,0 +1,23 @@
+<?php
+namespace Sol\Controllers;
+
+use Phink\Web\UI\TControl;
+
+class 1 extends TControl
+{
+    
+       
+	public function createObjects() {
+
+	}
+
+	public function declareObjects() {
+
+	}
+
+	public function displayHtml() {
+?>
+
+<?php
+	}
+}
