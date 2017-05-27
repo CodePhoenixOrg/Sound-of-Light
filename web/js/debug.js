0a266161e04649a3cac213fb327bd1379612e6c4
David Blanchard <dblanchard1@bbox.fr>
2017-05-27 19:31:30 +0200

    Simplify application tree

--- web/js/debug.js
+++ web/js/debug.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+Phink.DOM.ready(function () {
     var body = document.getElementsByTagName('body')[0];
     var div = document.createElement('div');
     div.setAttribute('id', 'debug');
